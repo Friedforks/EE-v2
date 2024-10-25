2f892c87d8a36d7b7761b136b9366a2b6e25ba48
Friedforks <3344544482@qq.com>
2024-10-26 00:11:32 +0800

    Final version

--- src/experiments/iTransformer/scripts/boost_performance/TCF1d-compare.sh
+++
@@ -1,5 +0,0 @@
-./scripts/boost_performance/Traffic/TCformer1dSplit.sh
-
-./scripts/boost_performance/ECL/TCformer1dSplit.sh
-
-./scripts/boost_performance/Weather/TCformer1dSplit.sh
--- src/experiments/iTransformer/scripts/boost_performance/Transformer-TCF1d-compare.sh
+++
@@ -1,5 +0,0 @@
-./scripts/boost_performance/Traffic/iTransformer.sh
-
-./scripts/boost_performance/ECL/iTransformer.sh
-
-./scripts/boost_performance/Weather/iTransformer.sh
